a108baf994741324662aa7f137614f360af12ac4
ajaykumar7w <100149926+ajaykumar7w@users.noreply.github.com>
2022-11-03 07:31:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aviatrix" {
   controller_ip = var.controller_ip
   username      = var.username
   password      = var.password
-  skip_version_validation = false
+  skip_version_validation = true
 }
 
 provider "aws" {
